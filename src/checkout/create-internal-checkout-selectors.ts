4481118b2b539f179f3510e1d263ba915800e8b2
David Chin <davidchin.yf@gmail.com>
2019-07-31 22:23:37 +1000

    perf(checkout): CHECKOUT-4272 Add function for creating internal checkout selectors factory

--- src/checkout/create-internal-checkout-selectors.ts
+++ src/checkout/create-internal-checkout-selectors.ts
@@ -18,7 +18,12 @@ import { CheckoutStoreOptions } from './checkout-store';
 import CheckoutStoreState from './checkout-store-state';
 import InternalCheckoutSelectors from './internal-checkout-selectors';
 
-export default function createInternalCheckoutSelectors(state: CheckoutStoreState, options: CheckoutStoreOptions = {}): InternalCheckoutSelectors {
+export type InternalCheckoutSelectorsFactory = (
+    state: CheckoutStoreState,
+    options?: CheckoutStoreOptions
+) => InternalCheckoutSelectors;
+
+export function createInternalCheckoutSelectorsFactory(): InternalCheckoutSelectorsFactory {
     const createBillingAddressSelector = createBillingAddressSelectorFactory();
     const createCartSelector = createCartSelectorFactory();
     const createCheckoutButtonSelector = createCheckoutButtonSelectorFactory();
@@ -41,53 +46,62 @@ export default function createInternalCheckoutSelectors(state: CheckoutStoreStat
     const createOrderSelector = createOrderSelectorFactory();
     const createPaymentSelector = createPaymentSelectorFactory();
 
-    const billingAddress = createBillingAddressSelector(state.billingAddress);
-    const cart = createCartSelector(state.cart);
-    const checkoutButton = createCheckoutButtonSelector(state.checkoutButton);
-    const config = createConfigSelector(state.config);
-    const countries = createCountrySelector(state.countries);
-    const coupons = createCouponSelector(state.coupons);
-    const customer = createCustomerSelector(state.customer);
-    const customerStrategies = createCustomerStrategySelector(state.customerStrategies);
-    const form = createFormSelector(state.config);
-    const giftCertificates = createGiftCertificateSelector(state.giftCertificates);
-    const instruments = createInstrumentSelector(state.instruments);
-    const paymentMethods = createPaymentMethodSelector(state.paymentMethods);
-    const paymentStrategies = createPaymentStrategySelector(state.paymentStrategies);
-    const shippingAddress = createShippingAddressSelector(state.consignments);
-    const remoteCheckout = createRemoteCheckoutSelector(state.remoteCheckout);
-    const shippingCountries = createShippingCountrySelector(state.shippingCountries);
-    const shippingStrategies = createShippingStrategySelector(state.shippingStrategies);
+    return (state, options = {}) => {
+        const billingAddress = createBillingAddressSelector(state.billingAddress);
+        const cart = createCartSelector(state.cart);
+        const checkoutButton = createCheckoutButtonSelector(state.checkoutButton);
+        const config = createConfigSelector(state.config);
+        const countries = createCountrySelector(state.countries);
+        const coupons = createCouponSelector(state.coupons);
+        const customer = createCustomerSelector(state.customer);
+        const customerStrategies = createCustomerStrategySelector(state.customerStrategies);
+        const form = createFormSelector(state.config);
+        const giftCertificates = createGiftCertificateSelector(state.giftCertificates);
+        const instruments = createInstrumentSelector(state.instruments);
+        const paymentMethods = createPaymentMethodSelector(state.paymentMethods);
+        const paymentStrategies = createPaymentStrategySelector(state.paymentStrategies);
+        const shippingAddress = createShippingAddressSelector(state.consignments);
+        const remoteCheckout = createRemoteCheckoutSelector(state.remoteCheckout);
+        const shippingCountries = createShippingCountrySelector(state.shippingCountries);
+        const shippingStrategies = createShippingStrategySelector(state.shippingStrategies);
 
-    // Compose selectors
-    const consignments = createConsignmentSelector(state.consignments, cart);
-    const checkout = createCheckoutSelector(state.checkout, billingAddress, cart, consignments, coupons, customer, giftCertificates);
-    const order = createOrderSelector(state.order, billingAddress, coupons);
-    const payment = createPaymentSelector(checkout, order);
+        // Compose selectors
+        const consignments = createConsignmentSelector(state.consignments, cart);
+        const checkout = createCheckoutSelector(state.checkout, billingAddress, cart, consignments, coupons, customer, giftCertificates);
+        const order = createOrderSelector(state.order, billingAddress, coupons);
+        const payment = createPaymentSelector(checkout, order);
 
-    const selectors = {
-        billingAddress,
-        cart,
-        checkout,
-        checkoutButton,
-        config,
-        consignments,
-        countries,
-        coupons,
-        customer,
-        customerStrategies,
-        form,
-        giftCertificates,
-        instruments,
-        order,
-        payment,
-        paymentMethods,
-        paymentStrategies,
-        remoteCheckout,
-        shippingAddress,
-        shippingCountries,
-        shippingStrategies,
+        const selectors = {
+            billingAddress,
+            cart,
+            checkout,
+            checkoutButton,
+            config,
+            consignments,
+            countries,
+            coupons,
+            customer,
+            customerStrategies,
+            form,
+            giftCertificates,
+            instruments,
+            order,
+            payment,
+            paymentMethods,
+            paymentStrategies,
+            remoteCheckout,
+            shippingAddress,
+            shippingCountries,
+            shippingStrategies,
+        };
+
+        return options.shouldWarnMutation ? createFreezeProxies(selectors) : selectors;
     };
+}
 
-    return options.shouldWarnMutation ? createFreezeProxies(selectors) : selectors;
+export default function createInternalCheckoutSelectors(
+    state: CheckoutStoreState,
+    options?: CheckoutStoreOptions
+): InternalCheckoutSelectors {
+    return createInternalCheckoutSelectorsFactory()(state, options);
 }
